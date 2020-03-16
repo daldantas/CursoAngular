5ffb41b0ff801c6333e5e3321202b86898ed9349
Gabriel Dantas Ferreira <biotoup@gmail.com>
2020-03-15 21:50:17 -0300

    Lesson 3 done.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Aula3Component } from './aula3/aula3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Aula3Component
   ],
   imports: [
     BrowserModule,
---
+++ src/app/aula3/aula3.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-aula3',
+  templateUrl: './aula3.component.html',
+  styleUrls: ['./aula3.component.css']
+})
+export class Aula3Component {
+  title = 'Aula 3 - Criando um componente'
+}
